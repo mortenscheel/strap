a4c08bf0d479a0a49112e143cf3c84c74dcd85c7
Morten Scheel <morten@mortenscheel.com>
2023-12-26 12:07:05 +0100

    Update Larastan

--- examples/js/larastan-strap.js
+++ examples/js/larastan-strap.js
@@ -2,43 +2,61 @@
  * @param {import('./types').StrapUtils} util
  * @returns {import('./types').Strap}
  */
-module.exports = util => ({
-    name: 'larastan',
-    description: 'Static analysis for Laravel',
-    skip: async () => (util.project.isLaravel() ? false : 'Larastan requires a Laravel project'),
+module.exports = (util) => ({
+    name: "larastan",
+    description: "Static analysis for Laravel",
+    skip: async () =>
+        util.project.isLaravel()
+            ? false
+            : "Larastan requires a Laravel project",
     context: async () => {
         const level = await util.inquirer.input({
-            message: 'Select level [1-9]',
-            default: '8',
-            validate: value => /^[1-9]$/.test(value),
+            message: "Select level [1-9]",
+            default: "8",
+            validate: (value) => /^[1-9]$/.test(value),
         });
         const checkMissingIterableValueType = await util.inquirer.confirm({
-            message: 'Check missing iterable value type',
+            message: "Check missing iterable value type",
             default: false,
         });
-        return {level, checkMissingIterableValueType};
+        return { level, checkMissingIterableValueType };
     },
     tasks: [
         {
-            title: 'Install Larastan',
+            title: "Install Larastan",
             skip: async () =>
-                (await util.project.hasComposerPackage('nunomaduro/larastan')) ? 'Larastan is already installed' : false,
-            task: () => util.execa('composer', ['require', 'nunomaduro/larastan', '--dev']),
+                (await util.project.hasComposerPackage("larastan/larastan"))
+                    ? "Larastan is already installed"
+                    : false,
+            task: () =>
+                util.execa("composer", [
+                    "require",
+                    "larastan/larastan",
+                    "--dev",
+                ]),
         },
         {
-            title: 'Generate phpstan.neon',
-            skip: () => (util.fs.existsSync('phpstan.neon') ? 'phpstan.neon already exists' : false),
-            task: ctx => {
+            title: "Generate phpstan.neon",
+            skip: () =>
+                util.fs.existsSync("phpstan.neon")
+                    ? "phpstan.neon already exists"
+                    : false,
+            task: (ctx) => {
                 const config = {
-                    includes: ['./vendor/nunomaduro/larastan/extension.neon'],
+                    includes: ["./vendor/larastan/larastan/extension.neon"],
                     parameters: {
-                        paths: ['app/'],
+                        paths: ["app/"],
                         level: parseInt(ctx.level),
                         excludePaths: [],
-                        checkMissingIterableValueType: ctx.checkMissingIterableValueType,
+                        checkMissingIterableValueType:
+                            ctx.checkMissingIterableValueType,
                     },
                 };
-                util.fs.writeFileSync('phpstan.neon', util.yaml.dump(config), 'utf-8');
+                util.fs.writeFileSync(
+                    "phpstan.neon",
+                    util.yaml.dump(config),
+                    "utf-8"
+                );
             },
         },
     ],
